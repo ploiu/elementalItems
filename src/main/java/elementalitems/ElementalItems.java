8642252cb4aa132801647fc22500d6e3eb2c75bb
ploiu <curtislanglitz+github@gmail.com>
2020-04-19 10:22:47 -0400

    fix crash associated with air crystal recipe

--- src/main/java/elementalitems/ElementalItems.java
+++ src/main/java/elementalitems/ElementalItems.java
@@ -28,7 +28,7 @@ import org.apache.logging.log4j.Logger;
 public class ElementalItems {
 	public static final String MOD_ID = "elementalitems";
 	public static final String NAME = "Elemental Items Mod";
-	public static final String VERSION = "1.9";
+	public static final String VERSION = "1.9.1";
 	//the mod instance;
 	@Mod.Instance(MOD_ID)
 	public static ElementalItems instance;
